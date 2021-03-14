bd4ce13292e3dca4d127f256310fa3ddb72d81c1
Marcelo Ludovico <maludovico@msn.com>
2021-03-14 11:10:24 -0300

    Dependency injection and nuget fixes

--- Salao Marcelo/Startup.cs
+++ Salao Marcelo/Startup.cs
@@ -1,11 +1,13 @@
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
-using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using Salao_Marcelo.Data;
+using Salao_Marcelo.Data.Interfaces;
+using Salao_Marcelo.Data.Repositories;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -26,6 +28,16 @@ namespace Salao_Marcelo
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllers();
+
+            // Dependency Injection
+            services.AddScoped<IAppointmentRepository, AppointmentRepository>();
+            services.AddScoped<ICashFlowRepository, CashFlowRepository>();
+            services.AddScoped<IClientRepository, ClientRepository>();
+            services.AddScoped<IProfessionalRepository, ProfessionalRepository>();
+            services.AddScoped<IServiceRepository, ServiceRepository>();
+            services.AddDbContext<Context>(options => 
+                options.UseSqlServer(Configuration.GetConnectionString("sqlserver"))
+            );
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
